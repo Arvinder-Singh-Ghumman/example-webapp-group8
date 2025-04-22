2ed36935ebdaa0536cd0d4217dbe9d5b15c0d6fb
arvindersinghghumman <you@example.com>
2025-04-22 04:50:19 -0600

    test to see if jenkins runs

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('🚀 Hello from Group 8 - TECH2102!');
+app.get("/", (req, res) => {
+  res.send("🚀 Hello from Group 8 - TECH2102!");
 });
 
 app.listen(port, () => {
   console.log(`App is running on http://localhost:${port}`);
 });
+//just to test jenkins runs
